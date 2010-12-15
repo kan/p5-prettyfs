929828f160cc7f14764086cd9147cd635912a962
Kan Fushihara <kan.fushihara@gmail.com>
2010-12-15 19:01:42 +0900

    first commit

---
+++ blib/lib/PrettyFS.pm
@@ -0,0 +1,39 @@
+package PrettyFS;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+PrettyFS -
+
+=head1 SYNOPSIS
+
+  use PrettyFS;
+
+=head1 DESCRIPTION
+
+PrettyFS is
+
+=head1 AUTHOR
+
+Kan Fushihara E<lt>kan dot fushihara at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kan Fushihara
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
