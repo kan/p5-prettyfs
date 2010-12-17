a520abbf6782ecaef71f2cbcff1099a655cde7b9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-17 17:39:41 +0900

    added PrettyFS::DBI

---
+++ lib/PrettyFS/DBI.pm
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+use utf8;
+
+package PrettyFS::DBI;
+use Class::Accessor::Lite (
+    new => 1,
+    ro  => [qw/dbh/],
+);
+
+sub search {
+    my ( $self, $stmt, @binds ) = @_;
+    my $x = $self->selectall_arrayref( $stmt, +{ Slice => +{} }, @binds );
+    return wantarray ? @$x : $x;
+}
+
+sub single {
+    my ( $self, $stmt, @binds ) = @_;
+    return $self->selectrow_hashref( $stmt, +{}, @binds );
+}
+
+1;
+
