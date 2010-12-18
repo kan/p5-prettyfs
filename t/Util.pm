87aa9d52234ad46284777dd23c2a9ee8b61a2acb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-18 12:51:13 +0900

    better testing utility

--- t/Util.pm
+++ t/Util.pm
@@ -4,14 +4,21 @@ use warnings;
 use utf8;
 use parent qw/Exporter/;
 use DBI;
+use PrettyFS::Client;
 
-our @EXPORT = qw/get_dbh/;
+our @EXPORT = qw/get_dbh get_client/;
 
 sub get_dbh {
-    my $dbh = DBI->connect("dbi:SQLite:", '', '', {RaiseError => 1}) or die DBI->errstr;
+    my $dsn = shift || 'dbi:SQLite:';
+    my $dbh = DBI->connect($dsn, '', '', {RaiseError => 1}) or die DBI->errstr;
     open my $fh, '<', 'sql/sqlite.sql' or die $!;
     $dbh->do($_) for grep /\S/, split /;/, do { local $/; <$fh>};
     return $dbh;
+}
+
+sub get_client {
+    my $dbh = get_dbh();
+    PrettyFS::Client->new(dbh => $dbh);
 }
 
 1;
