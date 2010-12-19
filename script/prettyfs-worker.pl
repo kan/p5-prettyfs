b47f47535d2e8e8983aeb2d7e359ae7c41c7883f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-19 18:05:38 +0900

    refactoring worker

--- script/prettyfs-worker.pl
+++ script/prettyfs-worker.pl
@@ -9,17 +9,16 @@ use DBI;
 use Log::Minimal;
 use Module::Load ();
 use Pod::Usage;
+use Config;
 
 my $interval = 1;
 my $max_job_count = 100;
 my $config_path = 'config.pl';
-my $max_workers = 1;
 my @functions;
 GetOptions(
     'interval=i' => \$interval,
     'max_job_count=i' => \$max_job_count,
     'c|config=s' => \$config_path,
-    'max_workers=i' => \$max_workers,
     'worker=s@' => \@functions,
 );
 pod2usage() unless @functions;
@@ -27,17 +26,6 @@ pod2usage() unless @functions;
 for (@functions) {
     Module::Load::load($_);
 }
-
-my $pm = Parallel::Prefork->new(
-    {
-        max_workers  => $max_workers,
-        trap_signals => {
-            TERM => 'TERM',
-            HUP  => 'TERM',
-            USR1 => undef,
-        }
-    }
-);
 
 die "missing configuration file: $config_path" unless -f $config_path;
 my $config = do $config_path or die "cannot load configuration file: $config_path : $@";
@@ -48,35 +36,74 @@ local $Log::Minimal::PRINT = sub {
     warn "$time [$$] [$type] $message at $trace\n";
 };
 
-while ( $pm->signal_received ne 'TERM' ) {
-    $pm->start and next;
-
-    my $dbh = DBI->connect(@$db_conf) or die "Cannot connect to database server: $DBI::errstr";
-    my $fetcher = Jonk::Worker->new($dbh, {functions => \@functions});
-    my $job_count = $max_job_count;
-    my %worker_cache;
-    while ($job_count) {
-        if (my $job = $fetcher->dequeue) {
-            my $worker = ($worker_cache{$job->{func}} ||= $job->{func}->new(dbh => $dbh));
-            my $uuid = $job->{arg} or die;
-            infof("working: $uuid");
-            try {
-                $worker->work($uuid);
-                infof("working successfully: $uuid");
-            } catch {
-                critff("worker dies: $_");
-            };
-            $job_count--;
-        } else {
-            debugf("sleeping $interval secs");
-            sleep $interval;
-        }
+my %signo;
+do {
+    defined $Config{sig_name} || die "No sigs?";
+    my $i;
+    for my $name (split(' ', $Config{sig_name})) {
+        $signo{$name} = $i;
+        $i++;
     }
+};
+my %pid_map;
+$SIG{TERM} = $SIG{INT} = sub {
+    # kill all children
+    kill keys %pid_map;
+    1 while wait()!=-1;
 
-    $pm->finish;
+    exit $signo{$_[0]} + 128;
+};
+for my $function (@functions) {
+    run_child($function);
 }
 
-$pm->wait_all_children();
+while (%pid_map) {
+    if (my $pid = wait()) {
+        my $function = delete $pid_map{$pid};
+        run_child($function);
+    }
+}
+
+sub run_child {
+    my $function = shift;
+    my $pid      = fork();
+    die $! unless defined $pid;
+
+    if ($pid) {
+        $pid_map{$pid} = $function;
+        return;
+    }
+    else {    # child
+        my $dbh = DBI->connect(@$db_conf)
+          or die "Cannot connect to database server: $DBI::errstr";
+
+        my $fetcher = Jonk::Worker->new( $dbh, { functions => \@functions } );
+        my $job_count = $max_job_count;
+        my %worker_cache;
+        my $worker = $function->new(dbh => $dbh);
+        while ($job_count) {
+            if ( my $job = $fetcher->dequeue ) {
+                infof("running job");
+
+                my $uuid = $job->{arg} or die;
+                infof("working: $uuid");
+                try {
+                    $worker->work($uuid);
+                    infof("working successfully: $uuid");
+                }
+                catch {
+                    critf("worker dies: $_");
+                };
+                $job_count--;
+            }
+            else {
+                debugf("sleeping $interval secs");
+                sleep $interval;
+            }
+        }
+        exit 0;
+    }
+}
 
 __END__
 
