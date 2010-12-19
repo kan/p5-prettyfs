1f1f6f30ef66041eb1d3462e65e928820de001ca
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-19 11:43:48 +0900

    normalized replication worker

--- script/prettyfs-worker.pl
+++ script/prettyfs-worker.pl
@@ -7,17 +7,26 @@ use Parallel::Prefork;
 use Getopt::Long;
 use DBI;
 use Log::Minimal;
+use Module::Load ();
+use Pod::Usage;
 
 my $interval = 1;
 my $max_job_count = 100;
 my $config_path = 'config.pl';
 my $max_workers = 1;
+my @functions;
 GetOptions(
     'interval=i' => \$interval,
     'max_job_count=i' => \$max_job_count,
     'c|config=s' => \$config_path,
     'max_workers=i' => \$max_workers,
+    'worker=s@' => \@functions,
 );
+pod2usage() unless @functions;
+@functions = map { "PrettyFS::Worker::$_" } @functions;
+for (@functions) {
+    Module::Load::load($_);
+}
 
 my $pm = Parallel::Prefork->new(
     {
@@ -43,18 +52,21 @@ while ( $pm->signal_received ne 'TERM' ) {
     $pm->start and next;
 
     my $dbh = DBI->connect(@$db_conf) or die "Cannot connect to database server: $DBI::errstr";
-    my $worker = Jonk::Worker->new($dbh, {functions => ['PrettyFS::Worker::Replication']});
+    my $fetcher = Jonk::Worker->new($dbh, {functions => \@functions});
     my $job_count = $max_job_count;
-    while ($job_count--) {
-        if (my $job = $worker->dequeue) {
+    my %worker_cache;
+    while ($job_count) {
+        if (my $job = $fetcher->dequeue) {
+            my $worker = ($worker_cache{$job->{func}} ||= $job->{func}->new(dbh => $dbh));
             my $uuid = $job->{arg} or die;
             infof("working: $uuid");
             try {
-                PrettyFS::Worker::Replication->work($uuid);
+                $worker->work($uuid);
                 infof("working successfully: $uuid");
             } catch {
                 critff("worker dies: $_");
             };
+            $job_count--;
         } else {
             debugf("sleeping $interval secs");
             sleep $interval;
@@ -65,4 +77,10 @@ while ( $pm->signal_received ne 'TERM' ) {
 }
 
 $pm->wait_all_children();
+
+__END__
+
+=head1 SYNOPSIS
+
+    % prettyfs-worker --worker Repilcation
 
