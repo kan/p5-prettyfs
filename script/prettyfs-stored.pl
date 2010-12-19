d961e547d606d7ff49adda096f4b3f03df10d36c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-19 10:33:37 +0900

    run PrettyFS::DiskUsage on child process of Storage node

--- script/prettyfs-stored.pl
+++ script/prettyfs-stored.pl
@@ -5,12 +5,25 @@ use utf8;
 use PrettyFS::Server::Store;
 use Plack::Loader;
 use opts;
+use PrettyFS::DiskUsage;
+use Proc::Guard;
+use Log::Minimal;
 
 opts my $port => {default => 1919},
      my $base => {isa => 'Str', required =>1};
 
 my $app = PrettyFS::Server::Store->new(base => $base)->to_app();
 
-print "access to http://localhost:$port/\n";
+infof("Parent process: $$");
+
+my $disk_usage = Proc::Guard->new(
+    code => sub {
+        infof("Disk Usage worker: $$");
+        $0 = "prettyfs [disk-usage]";
+        PrettyFS::DiskUsage->new(docroot => $base)->run();
+    }
+);
+
+infof "access to http://localhost:$port/\n";
 Plack::Loader->load('Twiggy', port => $port)->run($app);
 
