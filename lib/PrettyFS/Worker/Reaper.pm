af54cf648bb2838ae9ae3f349a54f3794230dc42
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-20 11:03:08 +0900

    throttling reaper

--- lib/PrettyFS/Worker/Reaper.pm
+++ lib/PrettyFS/Worker/Reaper.pm
@@ -4,10 +4,11 @@ use warnings FATAL => 'all';
 use utf8;
 use Class::Accessor::Lite (
     new => 0,
-    ro => [qw/dbh jonk/],
+    ro => [qw/dbh jonk limit load/],
 );
 use Log::Minimal;
 use PrettyFS::Constants;
+use Sub::Throttle qw/throttle/;
 
 sub new {
     my $class = shift;
@@ -16,6 +17,8 @@ sub new {
         Carp::croak("missing mandatory parameter: $_") unless exists $args{$_};
     }
     $args{jonk} ||= Jonk::Client->new($args{dbh});
+    $args{load} ||= 1; # load 1 sec
+    $args{limit} ||= 1000;
     bless {
         %args
     }, $class;
@@ -34,15 +37,22 @@ sub run {
         Carp::croak "unknown dead storage: $host $port";
     }
 
-    my $sth = $self->dbh->prepare(q{SELECT file_uuid FROM file_on WHERE storage_id=?});
-    $sth->execute($storage_id);
-    while (my ($uuid) = $sth->fetchrow_array) {
-        $self->jonk->enqueue(
-            'PrettyFS::Worker::Replication',
-            $uuid
-        ) or Carp::croak $self->jon->errstr;
+    my $sth = $self->dbh->prepare(q{SELECT file_uuid FROM file_on WHERE storage_id=? LIMIT } . $self->limit);
+    while (1) {
+        infof("running reaper: $host_port");
+        throttle($self->load(), sub {
+            $sth->execute($storage_id);
+            while (my ($uuid) = $sth->fetchrow_array) {
+                $self->jonk->enqueue(
+                    'PrettyFS::Worker::Replication',
+                    $uuid
+                ) or Carp::croak $self->jonk->errstr;
 
-        $self->dbh->do(q{DELETE FROM file_on WHERE file_uuid=? AND storage_id=?}, {}, $storage_id, $uuid);
+                $self->dbh->do(q{DELETE FROM file_on WHERE file_uuid=? AND storage_id=?}, {}, $uuid, $storage_id)==1 or Carp::croak("Cannot delete row: " . $self->dbh->errstr);
+            }
+            $sth->finish();
+        });
+        last if $sth->rows == 0;
     }
 }
 
