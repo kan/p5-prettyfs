d8b68935b9341290abcdd965a82cd52aad8bbe05
Keiji, Yoshimi <walf443@gmail.com>
2011-01-17 22:44:45 +0900

    schema of mysql follow to a schema of sqlite.

--- sql/my.sql
+++ sql/my.sql
@@ -7,16 +7,33 @@ create table bucket (
 
 create table file (
     uuid       varchar(255)  not null primary key,
+    bucket_id  int unsigned not null,
+    size       int unsigned not null,
+    ext        varchar(10)
+) ENGINE=InnoDB;
+
+create table file_on (
+    file_uuid  varchar(255) not null,
     storage_id int unsigned not null,
-    bucket_id  int unsinged not null,
-    size       int unsigned not null
+    primary key (file_uuid, storage_id)
 ) ENGINE=InnoDB;
 
 create table storage (
     id       int unsigned not null primary key auto_increment,
-    hostname varchar(255) binary not null,
+    host     varchar(255) binary not null,
     port     int          unsigned not null,
-    status   TINYTINT     UNSIGNED NOT NULL DEFAULT 1,
-    unique (hostname, port)
+    status   TINYINT      UNSIGNED NOT NULL DEFAULT 1,
+    disk_total int unsigned default null,
+    disk_used int unsigned default null,
+    unique (host, port)
 ) ENGINE=InnoDB;
+
+-- Jonk
+
+create TABLE job (
+    id       int unsigned not null primary key auto_increment,
+    func     text,
+    arg      text,
+    enqueue_time text
+);
 
